130bf367a2278eb0f3e661d8ffca1e241ae560fe
Ozwald <asmundurg@gmail.com>
2023-01-23 19:54:23 +0000

    postprocessing added to fix how the language model sometimes breaks up words into smaller tokens

--- src/bert/api/model/code/model_wrapper.py
+++ src/bert/api/model/code/model_wrapper.py
@@ -10,6 +10,38 @@ def chunks(lst, n):
 
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 
+def post_process(sequence, tokens, offset_mappings, outcomes):
+  def remove_index(index, tokens, outcomes):
+    assert index > 0
+    removed_token = tokens.pop(index)
+    tokens[index - 1] += removed_token
+    # this code assumes the model is smart enough to classify each token
+    # when a word is broken up into smaller tokens, with the same tag.
+    # but may require testing, the following code may help with that.
+    # TEST CODE BELOW
+    # removed_outcome = outcomes.pop(index)
+    # assert outcomes[index-1] == removed_outcome
+    # ATTENTION, MAKE SURE TO COMMENT OUT outcomes.pop(index) HERE BELOW
+    # IF USING THE COMMENTED OUT TEST CODE HERE ABOVE
+    outcomes.pop(index)
+
+  def fix_tokens(indices, sequence, tokens, offset_mappings):
+    for i in indices:
+      lower_bound, upper_bound = offset_mappings[i]
+      tokens[i] = sequence[lower_bound:upper_bound]
+
+  # find the indices of the extra tokens, if it begins in the same index as the
+  # previous item, (offset_mapping), it's either a punctuation, or, an 'extra-token',
+  # and then you distinguish between the two because the length of the extra token
+  # is longer than the length of its corresponding offset map.
+  indices = [i for i, (a, b) in enumerate(offset_mappings) if
+             i > 0 and a == offset_mappings[i - 1][1] and
+             offset_mappings[i - 1][1] - offset_mappings[i - 1][0] != 0 and
+             len(tokens[i]) != offset_mappings[i][1] - offset_mappings[i][0]]
+  fix_tokens(indices, sequence, tokens, offset_mappings)
+  # remove indices from list in reversed order
+  for i in indices[::-1]:
+    remove_index(i, tokens, outcomes)
 
 class ModelWrapper():
   def __init__(self, model, tokenizer, label_dict, max_length):
@@ -29,12 +61,18 @@ class ModelWrapper():
 
       sequence = ' '.join(sequence_splits)
       tokens = self._tokenizer.tokenize(self._tokenizer.decode(self._tokenizer.encode(sequence)))
-      inputs = self._tokenizer.encode(sequence, return_tensors="pt")
+      encoded_data = self._tokenizer.encode_plus(sequence, return_tensors="pt", add_special_tokens=True,
+                                                 max_length=self._MAX_LENGTH,
+                                                 return_offsets_mapping=True)
+      inputs = encoded_data['input_ids']
 
       outputs = self._model(inputs)[0]
 
       predictions = torch.argmax(outputs, dim=2)
       outcomes = [self._label_dict[str(prediction)] for prediction in predictions[0].tolist()]
+      offset_mappings_numpy = encoded_data['offset_mapping'].numpy()
+      offset_mappings = [tuple(x) for x in offset_mappings_numpy[0]]
+      post_process(sequence, tokens, offset_mappings, outcomes)
       all_predictions.extend(outcomes)
       all_tokens.extend(tokens)
 
